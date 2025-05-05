38a138739cc5a0761be12089d1df9ae17dd338ad
Johanx22x <johanrodsa2210@gmail.com>
2025-05-04 23:22:52 -0600

    Update to support ts and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        ¡Hola, Tailwind + React + TS + Vite!
+      </h1>
+    </div>
+  )
+}
+
+export default App
